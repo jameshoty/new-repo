6f7a95df2eec9ecf0c76f426293da7afd856362b
James HO <31378190+jameshoty@users.noreply.github.com>
2019-02-27 16:49:52 +0800

    Update newfile.php

--- newfile.php
+++ newfile.php
@@ -2,4 +2,6 @@
 // This is my first PHP to Github
 echo "Hello world!";
 echo "It is getting more exciting";
+
+// This comment is entered at Gibhub page to demo how I can get updated at the Eclipse
 ?>
